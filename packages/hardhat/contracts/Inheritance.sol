a33e8ed2e15e7d1586f23d05802ee1bb9d2aa1f3
bagusino <jglz0001@red.ujaen.es>
2025-02-26 22:29:09 +0100

    - Added some comentaries. - TODO is the tag to follow. - Changed InheritanceFactory with the correct file content

--- packages/hardhat/contracts/Inheritance.sol
+++ packages/hardhat/contracts/Inheritance.sol
@@ -2,36 +2,65 @@
 pragma solidity >=0.8.0 <0.9.0;
 
 import "@openzeppelin/contracts/access/Ownable.sol";
-
+/**
+ * @title Inheritance
+ * @author bagusino
+ * @notice This contract aims to be used by a web platform to deploy dinamically inheritance contracts
+ * TODO: Destroy the contract after the inheritance has been done
+ */
 contract Inheritance is Ownable {
-    uint256 public lastCheckIn;
-    uint256 public checkInInterval = 365 days;
-    mapping(address => bool) public isHeir;
-    address[] public heirs;
 
-    event FundsDeposited(address indexed from, uint256 amount);
-    event InheritanceDistributed();
+    uint256 public lastCheckIn; //Last time when the owner logged in
+    uint256 public checkInInterval = 365 days; // Interval of days until inherit functionality is active
+    mapping(address => bool) public isHeir; // Mapping of heirs
+    address[] public heirs; // Array of heirs
 
-    constructor(address _owner) Ownable(_owner) {
+    event FundsDeposited(address indexed from, uint256 amount); // Event to notify that funds are deposited
+    event InheritanceDistributed(); // Event to notify that the inheritance has been done
+    /*  
+    * @notice Constructor of the contract
+    * @param _owner The owner of the contract
+    * @param _heirs The heirs of the contract 
+    * TODO: Add require to check that the owner is not an heir
+    *       Improve gas efficiency by using a mapping for heirs
+    */
+    constructor(address _owner, address[] memory _heirs) Ownable(_owner) {
         lastCheckIn = block.timestamp;
+        for (uint256 i = 0; i < _heirs.length; i++) {
+            require(_heirs[i] != _owner, "Owner cannot be an heir");
+            require(!isHeir[_heirs[i]], "Heir already exists");
+            isHeir[_heirs[i]] = true;
+            heirs.push(_heirs[i]);
+        }
     }
-
+    /*
+    * @notice Function to check in the owner
+    */
     function checkIn() public onlyOwner {
         lastCheckIn = block.timestamp;
     }
-
-    function addHeir(address heir) public onlyOwner {
-        require(heir != msg.sender, "Owner cannot be an heir");
-        require(!isHeir[heir], "Heir already exists");
-        isHeir[heir] = true;
-        heirs.push(heir);
+    /*
+    * @notice Function to add an heir
+    * @param _heir The address of the heir
+    */
+    function addHeir(address _heir) public onlyOwner {
+        require(_heir != msg.sender, "Owner cannot be an heir");
+        require(!isHeir[_heir], "Heir already exists");
+        isHeir[_heir] = true;
+        heirs.push(_heir);
     }
-
-    function removeHeir(address heir) public onlyOwner {
-        require(isHeir[heir], "Heir not found"); // TBD check if this is neccesary
-        isHeir[heir] = false;
+    /*
+    * @notice Function to remove an heir
+    * @param _heir The address of the heir
+    * TODO: Remove the heir from the heirs array. 
+    *      Take in account the gas efficiency
+    */
+    function removeHeir(address _heir) public onlyOwner {
+        isHeir[_heir] = false;
     }
-
+    /*
+    * @notice Function to distribute the inheritance that the heirs can use to withdraw the funds
+    */
     function distributeInheritance() public {
         require(block.timestamp > lastCheckIn + checkInInterval, "Owner is still alive");
         uint256 balance = address(this).balance;
@@ -43,22 +72,33 @@ contract Inheritance is Ownable {
         }
         emit InheritanceDistributed();
     }
-
+    /*
+    * @notice Function to deposit funds to the contract
+    */
     receive() external payable {
         require(msg.value > 0, "Must send ETH");
         emit FundsDeposited(msg.sender, msg.value);
     }
+    /*
+    * @notice Function to deposit the funds of the contract
+    * TODO: Think about if this function will be available only by the owner
+    */
     function deposit() external payable {
         require(msg.value > 0, "Must send ETH");
         emit FundsDeposited(msg.sender, msg.value);
     }
+    /*
+    * @notice Function to withdraw the funds of the contract to the owner
+    */
     function withdraw() external onlyOwner {
         require(block.timestamp > lastCheckIn + checkInInterval, "Owner is still alive");
         payable(owner()).transfer(address(this).balance);
     }
+    /*
+    * @notice Function to get the balance of the contract
+    */
     function getBalance() public view returns (uint256) {
         return address(this).balance;
     }
-
 
 }
--- packages/hardhat/contracts/InheritanceFactory.sol
+++ packages/hardhat/contracts/InheritanceFactory.sol
@@ -3,26 +3,28 @@ pragma solidity >=0.8.0 <0.9.0;
 
 import "./Inheritance.sol";
 
+/**
+ * @title InheritanceFactory
+ * @notice This contract aims to be used by a web platform to deploy dinamically inheritance contracts
+ */
+
 contract InheritanceFactory {
-    event InheritanceCreated(address indexed owner, address inheritanceContract);
+    event InheritanceCreated(address indexed owner, address inheritanceContract); // Event to notify that an inheritance contract has been created
 
-    mapping(address => address) public userContracts;
-    address[] public allContracts;
+    mapping(address => address) public userContracts; // Mapping of the users and their contracts
 
-    function createInheritanceContract() public {
-        require(userContracts[msg.sender] == address(0), "User already has an inheritance contract");
-
-        Inheritance newContract = new Inheritance(msg.sender);
+    function createInheritanceContract(address[] memory _heirs) public {
+        Inheritance newContract = new Inheritance(msg.sender, _heirs);
         userContracts[msg.sender] = address(newContract);
-        allContracts.push(address(newContract));
-
         emit InheritanceCreated(msg.sender, address(newContract));
     }
-
-    function getAllContracts() public view returns (address[] memory) {
-        return allContracts;
-    }
-
+    /*
+    * @notice Function to get the contract of a user
+    * @param user The address of the owner
+    * @return The address of the contract of the user
+    * TODO: Check if there are more than 1 contract if are all returned. 
+    * If I believe the work of a mapping it will only return 1 contract (tree structure)
+    */
     function getUserContract(address user) public view returns (address) {
         return userContracts[user];
     }
